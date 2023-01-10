6b378276fc46eb8628e2e53ba3d56def92c90a2f
zborna <zborna@users.noreply.github.com>
2023-01-10 21:05:59 +0100

    changes to layout of ERD

--- dbml2dot/generators.py
+++ dbml2dot/generators.py
@@ -6,30 +6,34 @@ import pydot
 from dbml2dot.utils import debug
 
 
+
 def generate_table_label(name: str, attributes: list[str]):
     attribute_list: list[str] = []
     for attr in attributes:
-        attribute_list += [f"""<TR><TD align="left">{attr}</TD></TR>"""]
+        if (str(attr).startswith('<TD port=')):
+            attribute_list += [f"""<TR>{attr}</TR>"""]
+        else:
+            attribute_list += [f"""<TR><TD align="left">{attr}</TD></TR>"""]
     attribute_list_str = "\n".join(attribute_list)
     return dedent(f'''
         <<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="1">
-        <TR><TD><B>{name}</B></TD></TR><HR></HR>
+        <TR><TD><I>{name}</I></TD></TR><HR></HR>
         {attribute_list_str}
         </TABLE>>''').strip().replace('\n', '\n\t')
 
 
 def generate_column_node(name: str, column_attributes: pydbml.classes.Column, enums: list[str]):
-    not_null_string = "" if column_attributes.not_null or column_attributes.pk else "?"
-    col_string = f"{name}{not_null_string}"
+    col_string = f'{name}'
     if column_attributes.pk:
         col_string = f"<B>{col_string}</B>"
     if column_attributes.unique:
         col_string = f"<I>{col_string}</I>"
-    attribute_str = f"{col_string} : {column_attributes.type}"
+    attribute_str = f'<TD port="{name}" align="left" cellpadding="5">{col_string} : <font color="grey60">{column_attributes.type}</font></TD>'
 
     enums_used = []
     if str(column_attributes.type).strip() in enums:
         enums_used += [str(column_attributes.type)]
+        
 
     return attribute_str, enums_used
 
@@ -63,11 +67,30 @@ def generate_table_nodes(name: str, contents: pydbml.classes.Table, enums: list[
     return node, edges
 
 
-def generate_graph_from_dbml(dbml: pydbml.PyDBML) -> pydot.Graph:
-    graph = pydot.Graph()
-    graph.set_node_defaults(fontname="Bitstream Vera Sans", fontsize=8, shape="none")
-    graph.set_edge_defaults(fontname="Bitstream Vera Sans", fontsize=8, labeldistance=2, color="grey")
-    graph.set_graph_defaults(rankdir='LR')
+def generate_graph_from_dbml(dbml: pydbml.PyDBML,name:str) -> pydot.Dot:
+    #graph = pydot.Graph()
+    graph = pydot.Dot() #change to Dot to enable export
+
+    # using styles from https://github.com/ehne/ERDot
+    graph.set_node_defaults(fontname="Helvetica",shape="none")
+    graph.set_edge_defaults(
+        dir="both",
+        fontsize=12,
+        arrowsize=0.9,
+        penwidth=1.0,
+        labelangle=32,
+        labeldistance=1.8,
+        fontname="Helvetica")
+    graph.set_graph_defaults(
+        nodesep=0.5,
+        rankdir="LR",
+        concentrate="true",
+        splines="spline",
+        fontname="Helvetica",
+        pad="0.2,0.2",
+        label=f"<<font point-size='30'>{name}</font>>",
+        labelloc = "t"
+    )
     enums = []
     for enum in dbml.enums:
         enum: pydbml.classes.Enum = enum
@@ -103,14 +126,17 @@ def generate_graph_from_dbml(dbml: pydbml.PyDBML) -> pydot.Graph:
                 graph.add_edge(pydot.Edge(reference.table1.name, reference.table2.name, style="invis"))
                 # graph.
 
+
+
+
         graph.add_edge(pydot.Edge(
             reference.table1.name, reference.table2.name,
-            headlabel=dest,
-            taillabel=orig,
-            xlabel=" " * label_len*2,
-            minlen=int(math.sqrt(1+label_len/2))
-            # xlabel=f"{reference.col1.name} {reference.type} {reference.col2.name}"
-        ))
+            headport=dest,
+            tailport=orig,
+            arrowhead="noneotee",
+            arrowtail="ocrow"))
+        
+        
 
     # graph.set_simplify(True)
     graph.set_type("digraph")
